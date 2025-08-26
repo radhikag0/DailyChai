9fcce3bd47140a8ecbe419eba5a334724fb95002
radhikag0 <gargradhika2004@gmail.com>
2025-08-26 22:49:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import {useUser} from '@clerk/clerk-react'
+import { Layout } from 'lucide-react'
+
+const App = () => {
+  const {user} = useUser()
+  return (
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      {/* <Route path="/" element={ <Login /> : <Layout/>} /> */}
+      {/* <Route index element={<Feed />} /> */}
+      <Route path="messages" element={<Messages />} />
+      <Route path="messages/:userId" element={<ChatBox />} />
+      <Route path="connections" element={<Connections />} />
+      <Route path="discover" element={<Discover />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="profile/:profileId" element={<Profile />} />
+      <Route path="create-post" element={<CreatePost />} />
+    </Routes>
+  )
+}
+
+export default App
