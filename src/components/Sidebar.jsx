7f7e07582ffaf68e514a2e076911df9031d157ba
radhikag0 <gargradhika2004@gmail.com>
2025-09-05 21:07:39 +0530

    Connecttion, Messages and Discover Page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
 
   return (
     <div className= {`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col
-    justify-between items-center max-sm:abslute top-0 bottom-0 z-20 $
+    justify-between items-center max-sm:absolute top-0 bottom-0 z-20 $
     {sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'}
     transition-all duration-300 ease-in-out`}>
         <div className='w-full'>
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import Layout from './Layout'
 import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
+import RecentMessages from '../components/RecentMessages'
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([])
@@ -24,13 +26,21 @@ const Feed = () => {
       <div>
         <StoriesBar/>
         <div className='p-4 space-y-6'>
-          List of post
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post}/>
+          ))}
         </div>
       </div>
       {/* right side bar */}
-      <div>
-          <div><h1>Sponsored</h1></div>
-          <h1>Recent Messages</h1>
+      <div className='sticky top-0'>
+          <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex
+          flex-col gap-2 shadow'>
+            <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+            <img src={assets.sponsored_img} className="w-75 h-50 rounded-md" alt="Sponsored Ad" />
+            <p className='text-slate-600'>Email Marketing</p>
+            <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
+            </div>
+          <RecentMessages/>
       </div>
     </div>
   ) : <Loading/>;
